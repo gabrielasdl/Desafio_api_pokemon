38574f644fbbfe829d134b3a9474e5239e7f8a06
Gabriela Leite <gabi_leitte@hotmail.com>
2020-07-01 15:36:27 -0300

    coloquei explicaçoes em cada linha de código para ficar de facil entendimento

--- pokeapi.js
+++ pokeapi.js
@@ -4,27 +4,26 @@ const fs = require('fs')
 
 var pokemon = rs.question("Informe o POKEMON ou o ID: ")
 
-async function main() {
+async function main() { //função principal do programa 
 
     var pokemonRecuperado = await recuperaPokemon(pokemon) // a variavel esta recebendo o retorno da funcao que recupera os dados do pokemon
 
     exibeDadosPokemon(pokemonRecuperado) // vai imprimir os dados recuperados  
 
-    var salvarPokemon = rs.keyInYN("\nDeseja salvar na pokedex ")
+    var salvarPokemon = rs.keyInYN("\nDeseja salvar na pokedex ") 
 
     if (salvarPokemon == true) {
         salvaPokedex(pokemonRecuperado) // executa a funcao que salvo os dados do pokemon no arq json
     }
 }
 
-
 async function recuperaPokemon(pokemon) { // essa funcao serve para recuperar os dados do pokemon informado e retorna essas informacoes no obj criado -(recuperado)
     var response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     var recuperado = {} // objeto
-    recuperado.name = response.data.name
-    recuperado.abilities = response.data.abilities
-    recuperado.types = response.data.types
-    return recuperado
+    recuperado.name = response.data.name  // recebe nome do pokemon
+    recuperado.abilities = response.data.abilities  // recebe habilidade do pokemon
+    recuperado.types = response.data.types  // recebe tipo do pokemon
+    return recuperado  // retorna todos os dados recuperados
 }
 
 function salvaPokedex(pokemonRecuperado) { //essa funcao serializa o pokemon no arquivo json
@@ -33,17 +32,16 @@ function salvaPokedex(pokemonRecuperado) { //essa funcao serializa o pokemon no 
     pokemons.push(pokemonRecuperado)  // acrescenta o novo pokemon no array (arq json)
     var serializacaoPokemon = JSON.stringify(pokemons) // serializa os dados recebidos
     fs.writeFileSync('pokedex.json', serializacaoPokemon) // envia para o arquivo json
-
 }
 
-function exibeDadosPokemon(recuperaPokemon) {
+function exibeDadosPokemon(recuperaPokemon) { // essa função mostra as habilidades e os tipos 1 por 1
     console.log("Pokemon:", recuperaPokemon.name.toUpperCase())
     console.log("\nHabilidades:")
-    recuperaPokemon.abilities.forEach(abilityAtual => {
+    recuperaPokemon.abilities.forEach(abilityAtual => { // esse forEach percorre toda a lista de habilidades 
         console.log(abilityAtual.ability.name)
     });
     console.log("\nTipos:")
-    recuperaPokemon.types.forEach(typeAtual => {
+    recuperaPokemon.types.forEach(typeAtual => { // esse forEach percorre toda a lista de tipo
         console.log(typeAtual.type.name)
     })
 }
